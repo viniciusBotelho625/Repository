3b800ea6905169881a808cb310b24b190a301583
viniciusBotelho625 <viniciusoficialbotelho@outlook.com>
2024-02-14 11:06:41 -0300

    Menu

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -4,28 +4,30 @@ import '../../styles/sidebar.scss';
 import ToggleButton from './ToggleButton';
 import Links from './Links';
 
+
+const variants = {
+    open: {
+        clipPath: "circle(1200px at 50px 50px)",
+        transition: {
+            type: "spring",
+            stiffness: 20,
+        },
+    },
+    closed: {
+        clipPath: "circle(30px at 50px 50px)",
+        transition: {
+            delay: 0.5,
+            type: "spring",
+            stiffness: 400,
+            damping: 40,
+        },
+    },
+};
+
 export default function SideBar() {
 
     const [open, setOpen] = useState(false);
 
-    const variants = {
-        open: {
-            clipPath: "circle(1200px at 50px 50px)",
-            transition: {
-                type: "spring",
-                stiffness: 20,
-            },
-        },
-        closed: {
-            clipPath: "circle(30px at 50px 50px)",
-            transition: {
-                delay: 0.5,
-                type: "spring",
-                stiffness: 400,
-                damping: 40,
-            },
-        },
-    };
 
     return(
         <motion.div className='sidebar' animate={open ? "open" : "closed"}>
